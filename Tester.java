3d9af89ef8f85e73039fbdeafb9b59bb95339691
vasundhara_patil <73222853+vasundhara-patil@users.noreply.github.com>
2020-10-21 18:35:09 +0530

    Add files via upload

---
+++ Tester.java
@@ -0,0 +1,20 @@
+package com.pro;
+
+public class Tester {
+
+	public static void main(String[] args) {
+      Product p1=new Product(1,"abc",3,500);
+      System.out.println(p1);
+      Item i1=new Item();
+      System.out.println(i1);
+      Order o1 =new Order();
+      System.out.println(o1);
+      Customer c1=new Customer();
+      System.out.println(c1);
+      ShoppingCart s1=new ShoppingCart();
+      System.out.println(s1);
+      
+
+	}
+
+}
